924e9ad89c946afd7cd66bc177187ca71bf4aa13
Abderrahmanedev23 <125931479+Abderrahmanedev23@users.noreply.github.com>
2025-05-12 01:00:53 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/common.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// Add Google Fonts
+const link = document.createElement('link');
+link.href = 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap';
+link.rel = 'stylesheet';
+document.head.appendChild(link);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
